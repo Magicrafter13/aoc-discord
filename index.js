859781986723461082c6b9cc268883348c98810e
Matthew Rease <scubaventure101@hotmail.com>
2021-11-28 17:53:14 -0800

    Slash commands

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
+
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const command_files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of command_files) {
+	const command = require(`./commands/${file}`);
+	/*
+	 * Set a new item in the collection
+	 * With the key as the command name and the value as the exported module
+	 */
+	client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -14,18 +28,17 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand())
 		return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	switch (commandName) {
-		case "ping":
-			await interaction.reply("Pong!");
-			break;
-		case "server":
-			await interaction.reply("Server info.");
-			break;
-		case "user":
-			await interaction.reply("User info.");
-			break;
+	if (!command)
+		return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
